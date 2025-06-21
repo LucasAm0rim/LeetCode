289ed1bf0872308417d83b2840d5ac24f76759ed
ls1x <luc4ss00a@gmail.com>
2025-06-21 19:20:40 -0300

    1672. BETA v2.0 - The code is now fully functional standalone.

--- 1672-Richest-Customer-Wealth/1672.c
+++ 1672-Richest-Customer-Wealth/1672.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+// This is probably my ugliest code so far;
+// It is possible to do it in a much cleaner way, but for now thats enough;
+// Ideas: Not asking the user the AccountsSize and AccountsColSize, only the AccountsValue.
 
 int maximumWealth(int** accounts, int accountsSize, int accountsColSize) {
     int i = 0;
@@ -27,49 +33,108 @@ int maximumWealth(int** accounts, int accountsSize, int accountsColSize) {
     return richest;
 }
 
+void print_usage(char *argv[]){
+    printf("Usage: %s -a <Accounts> -s <AccountsSize> -c <AccountsColumnSize>\n", argv[0]);
+    printf("Example: %s -a 1,2,3,4,5,6 -s 2 -c 3\n", argv[0]);
+}
+
 int main(int argc, char *argv[]){
 
     int richest = 0;
-    int accountsSize = 3;
-    int accountsColSize = 3;
-    int ** accounts;
-    
-    // Allocating memory for an array of ints, it must be sizeof(int *)
-    // It is not necessary to include + 1 byte for the NULL byte. This is only needed when dealing with strings
-    accounts = calloc(accountsSize, sizeof(int *));
-    if (accounts == NULL){
-        printf("Malloc Failed.\n");
-        return -1;
-    }
+    int accountsSize = 0;
+    int accountsColSize = 0;
+    int c;
 
-    // Allocating memory for each int that it will be inside the vector
-    for (int i = 0; i < accountsSize; i++){
-        accounts[i] = calloc(accountsColSize, sizeof(int));
-        if (accounts[i] == NULL){
-            printf("Malloc Failed (2).\n");
+    char * acc = NULL;
+    char * accSize = NULL;
+    char * accColSize = NULL;
+
+    while ((c = getopt(argc,argv,"a:s:c:")) != -1){
+        switch (c){
+            case 'a':
+                acc = optarg;
+                break;
+            case 's':
+                accSize = optarg;
+                break;
+            case 'c':
+                accColSize = optarg;
+                break;
+            case '?':
+                break;
+            
+            default:
+                return -1;
+        }
+    }
+    
+    if (acc && accSize && accColSize){
+        // Allocating memory for an array of ints, it must be sizeof(int *)
+        // It is not necessary to include + 1 byte for the NULL byte. 
+        // This is only needed when dealing with strings
+        int i = 0;
+        char *endptr;
+    
+        accountsSize = strtol(accSize, &endptr, 10);
+        accountsColSize = strtol(accColSize, &endptr, 10);
+        
+        int * arrayAccounts = calloc(accountsSize * accountsColSize, sizeof(int)); 
+        arrayAccounts[0] = strtol(strtok(acc,","), &endptr, 10);
+
+        i = 0;
+        for (i = 1; i < (accountsSize * accountsColSize); i++){
+            arrayAccounts[i] = strtol(strtok(NULL,","), &endptr, 10);
+        }
+        
+        // Creating multidimentional array accounts.
+        int ** accounts;
+        accounts = calloc(accountsSize, sizeof(int *));
+        if (accounts == NULL){
+            printf("Malloc Failed.\n");
             return -1;
         }
-    }
-
-    // Inserting random numbers just for testing
-    printf("Inserted Numbers: ");
-    int N = 10;
-    for (int i = 0; i < accountsSize; i++){
-        for (int j = 0; j < accountsColSize; j++){
-            accounts[i][j] = rand() % (N + 1);
-            printf("%d ", accounts[i][j]);
+    
+        // Allocating memory for each int that it will be inside the vector
+        for (i = 0; i < accountsSize; i++){
+            accounts[i] = calloc(accountsColSize, sizeof(int));
+            if (accounts[i] == NULL){
+                printf("Malloc Failed (2).\n");
+                return -1;
+            }
         }
-        printf("| ");
+        
+        // Inserting the data into array Accounts
+        int j = 0;
+        int k = 0;
+        printf("Accounts Size: %d \n", accountsSize);
+        printf("Accounts Col Size: %d\n", accountsColSize);
+        printf("Inserted Values: | ");
+        for (i = 0; i < accountsSize; i++){
+            for (j = 0; j < accountsColSize; j++){
+                 accounts[i][j] = arrayAccounts[k];
+                 printf("%d | ", accounts[i][j]);
+                 k++;
+            }
+        }
+        printf("\n");
+
+        // Inserting everything into the function maximumWealth
+        richest = maximumWealth(accounts, accountsSize, accountsColSize);
+    
+        // Cleanup
+        for (i = 0; i < accountsSize; i++){
+            free(accounts[i]);
+        }
+        free(accounts);
+   
+        // Printing
+        printf("Richest Customer: %d\n", richest);
+        return 0;
+    
+    } else {
+        printf("Accounts, AccountSize and AccountsColumnSize are required parameters.\n");
+        print_usage(argv);
+        return 0;
     }
-
-    richest = maximumWealth(accounts, accountsSize, accountsColSize);
-
-    // Cleanup
-    for (int i = 0; i < accountsSize; i++){
-        free(accounts[i]);
-    }
-    free(accounts);
-
-    printf("\nRichest Customer: %d\n", richest);
-    return 0;
+    
 }
--- 1672-Richest-Customer-Wealth/README.md
+++ 1672-Richest-Customer-Wealth/README.md
@@ -7,3 +7,13 @@
 ### BETA v1.1
 - You can now compile the code and it will fill the array accounts with random numbers;
 - Improvements in the output of the code.
+
+### BETA v2.0
+- Now the full code is written and it is possible to run it standalone.
+
+- *Compilation:*
+- `gcc 1672.c -o 1672.bin`
+
+- *Usage:*
+- `./1672.bin -a <Accounts> -s <AccountsSize> -c <AccountsColumnSize>`
+- `./1672.bin -a 1,2,3,4,5,6 -s 2 -c 3`
