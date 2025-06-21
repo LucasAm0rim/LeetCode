4a0a48ebf35d29d02d81d1d9856094ad36019155
ls1x <luc4ss00a@gmail.com>
2025-06-21 11:48:38 -0300

    Improvements on 1672, Beta v1.1

--- 1672-Richest-Customer-Wealth/1672.c
+++ 1672-Richest-Customer-Wealth/1672.c
@@ -1,4 +1,7 @@
-int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
+#include <stdio.h>
+#include <stdlib.h>
+
+int maximumWealth(int** accounts, int accountsSize, int accountsColSize) {
     int i = 0;
     int j = 0;
     int k = 0;
@@ -7,13 +10,11 @@ int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
     //int * wealth = calloc(accountsSize + 1, sizeof(int));
     
     for (i = 0; i < accountsSize; i++){
-        for (j = 0; j < *accountsColSize; j++){
+        for (j = 0; j < accountsColSize; j++){
             wealth[k] = accounts[i][j] + wealth[k]; 
         }
         k++;
     }
-
-    printf("\n");
 
     k = 0;
     richest = wealth[0];
@@ -25,4 +26,43 @@ int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
     }
 
     return richest;
+}
+
+int main(int argc, char *argv[]){
+
+    int richest = 0;
+    int accountsSize = 3;
+    int accountsColSize = 3;
+    int ** accounts;
+    
+    // Allocating memory for an array of ints, it must be sizeof(int *)
+    // It is not necessary to include + 1 byte for the NULL byte. This is only needed when dealing with strings
+    accounts = calloc(accountsSize, sizeof(int *));
+    if (accounts == NULL){
+        printf("Malloc Failed.\n");
+        return -1;
+    }
+
+    for (int i = 0; i < accountsSize; i++){
+        accounts[i] = calloc(accountsColSize, sizeof(int));
+        if (accounts[i] == NULL){
+            printf("Malloc Failed (2).\n");
+            return -1;
+        }
+    }
+
+    printf("Inserted Numbers: ");
+    int N = 10;
+    for (int i = 0; i < accountsSize; i++){
+        for (int j = 0; j < accountsColSize; j++){
+            accounts[i][j] = rand() % (N + 1);
+            printf("%d ", accounts[i][j]);
+        }
+        printf("| ");
+    }
+
+    richest = maximumWealth(accounts, accountsSize, accountsColSize);
+
+    printf("\nRichest Customer: %d\n", richest);
+    return 0;
 }
--- 1672-Richest-Customer-Wealth/README.md
+++ 1672-Richest-Customer-Wealth/README.md
@@ -1,5 +1,9 @@
 # 1672. Richest Customer Wealth - LeetCode
 
-### Current State: BETA
+### BETA v1.0
 - In the files above you have access only to the MaximumWealth function, which can be compiled as an object or shared library;
 - The full code of the lesson is still to be developed.
+
+### BETA v1.1
+- You can now compile the code and it will fill the array accounts with random numbers;
+- Improvements in the output of the code.
